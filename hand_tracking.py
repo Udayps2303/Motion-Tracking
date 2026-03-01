02583edd9e4b93c46491cad3aa9be7eb711a9662
Uday Pratap Singh <udayps2303@gmail.com>
2026-03-01 22:42:56 +0530

    hand detection

---
+++ hand_tracking.py
@@ -0,0 +1,42 @@
+import cv2
+import mediapipe.python.solutions.hands as mp_hands
+import mediapipe.python.solutions.drawing_utils as mp_drawing
+import mediapipe.python.solutions.drawing_styles as mp_drawing_styles
+
+def run_hand_tracking():
+    cam = cv2.VideoCapture(index=0)
+    with mp_hands.Hands(
+        model_complexity=0,
+        max_num_hands=2,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
+        # Frame capture loop
+        while cam.isOpened():
+            success, frame = cam.read()
+            if not success:
+                print("Empty camera frame.")
+                continue
+
+            # the BGR image to RGB.
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = hands.process(frame_rgb)
+
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(
+                        image = frame,
+                        landmark_list=hand_landmarks,
+                        connections=mp_hands.HAND_CONNECTIONS,
+                        landmark_drawing_spec=mp_drawing_styles.get_default_hand_landmarks_style(),
+                        connection_drawing_spec=mp_drawing_styles.get_default_hand_connections_style())
+            
+            # Display window and flip frame
+            cv2.imshow('Hand Tracking', cv2.flip(frame, 1))
+            # Quit on 'Q' key press
+            if cv2.waitKey(5) & 0xFF == ord('q'):
+                break
+    cam.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    run_hand_tracking()
